1bbdce3f6124bebec963a35130e05f1e11937056
Sam Raby <Sam@Sams-MacBook-Pro-4.local>
2018-11-29 16:54:49 -0500

    nav bar css fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,11 +17,6 @@ export default () =>
 <div>
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div className="container">
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i className="fa fa-bars"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <a className="nav-link js-scroll-trigger" href="#bio">About Us</a>
@@ -33,7 +28,6 @@ export default () =>
               <a className="nav-link js-scroll-trigger" href="#contact">Get In Touch</a>
             </li>
           </ul>
-        </div>
       </div>
     </nav>
 
