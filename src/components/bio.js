ff8680a556063c5ac0af16d5719cfbed235d90cd
Sam Raby <Sam@Sams-MBP-4.brl.nyc>
2018-12-19 17:44:35 -0500

    added markdown html to about section

--- src/components/bio.js
+++ src/components/bio.js
@@ -8,27 +8,25 @@ const Bio = () => (
         query {
           contentfulAboutUs {
             title
-            p1 {
-              p1
-            } 
+            p1{
+              childMarkdownRemark {
+                html
+              }
+            }
         }}
     `}
-    render={({
-      contentfulAboutUs: {
-        title,
-        p1: { p1 }
-      }
-    }) => (
+    render={data => (
       <section id="bio">
           <div className="container">
             <div className="row">
               <div className="col-lg-12 text-center">
-                <h2 className="section-heading">{title}</h2>
+                <h2 className="section-heading">{data.contentfulAboutUs.title}</h2>
                 <h3 className="section-subheading text-muted">
                   <br/>
                   <div>
-                    <p align="left"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {p1}
-                      </p>
+                    <p align="left"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                      <span dangerouslySetInnerHTML={{ __html: data.contentfulAboutUs.p1.childMarkdownRemark.html}} />
+                    </p>
                   </div>
                 </h3>
               </div>
